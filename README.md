a591cead25ad02cda9a57472e90e83ea56638aaa
Adrián Félix <guerofelix234@gmail.com>
2024-05-05 01:22:40 -0700

    Correccion

--- README.md
+++
@@ -1,2 +0,0 @@
-# TEAW---Agencia-Viajes-Tercer-Avance
-Repositorio principal del tercer avance de proyecto propuesto sobre una agencia de viajes, para la materia de Temas Emergentes de Aplicaciones Web, en ITSON.
--- routes/usuarioroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/registrar', (req, res) => {
-    res.sendFile(__dirname + '/registrar/registrar.html');
-});
-
-module.exports = router;
